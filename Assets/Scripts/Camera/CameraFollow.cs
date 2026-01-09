360c8fc69574ca1d9d531e60eda6ff31dad0a8e0
nkochhar7 <nkochhar7@gatech.edu>
2026-01-08 23:27:39 -0500

    Add player jump and camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offset;
+    public float smoothSpeed = 5f;
+    public float pixelPerUnit = 100f; //TODO: needs to be modified with updated assets
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = playerTransform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        
+        smoothedPosition.x = Mathf.Round(smoothedPosition.x * pixelPerUnit) / pixelPerUnit;
+        smoothedPosition.y = Mathf.Round(smoothedPosition.y * pixelPerUnit) / pixelPerUnit;
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,10 +5,55 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRb;
     public float speed; //please_speed_i_need_this.png
     public float input;
+    public SpriteRenderer spriteRenderer;
+    public float jumpForce;
+
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    public Transform feetPosition;
+    public float groundCheckCircle;
+
+    public float jumpTime = 0.35f;
+    public float jumpTimeCounter;
+    private bool isJumping;
 
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
+        if (input > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (input < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            playerRb.linearVelocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                playerRb.linearVelocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
     }
 
     void FixedUpdate()
